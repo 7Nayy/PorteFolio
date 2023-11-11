ac5409f64da68d64c07a486f4ec77105d04dd2f6
7Nayy <nadim.benkrouidem@gmail.com>
2023-11-11 01:23:25 +0100

    Changement name and link

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -12,23 +12,19 @@ type Presentation = {
 };
 
 const presentation: Presentation = {
-  mail: "maxencewolff.pro@gmail.com",
-  title: "Hi, I’m Maxence 👋",
+  mail: "nadim.benkrouidem@gmail.com",
+  title: "Hi, I’m Nadim 👋",
   // profile: "/profile.webp",
   description:
-    "Bonjour, i'm a *french frontend developer* with over *3 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
+    "Bonjour, i'm a *french studiant developer* with over *2 years* of web experience.",
   socials: [
     {
       label: "X",
-      link: "https://twitter.com/itsstormzz_",
-    },
-    {
-      label: "Bento",
-      link: "https://bento.me/m-wolff",
+      link: "https://twitter.com/nayy_kun",
     },
     {
       label: "Github",
-      link: "https://github.com/MaeWolff",
+      link: "https://github.com/7Nayy",
     },
   ],
 };
